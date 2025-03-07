f07ab825ad42a10dd64b08c22cc496f7086fdbbf
felipehcaixeta <felipehcaixeta@gmail.com>
2025-03-07 15:09:53 -0600

    Initial commit.

---
+++ hidden_menu_bar.js
@@ -0,0 +1,9 @@
+document.getElementById("MenuToggle").addEventListener("click", function () {
+  const AppDrawer = document.getElementById("AppDrawer");
+  const MainContent = document.getElementById("MainContent");
+  const ScrollableHeader = document.getElementById("ScrollableHeader");
+  AppDrawer.classList.toggle("menu-bar-open"); // Acess this class in the CSS file and assign it AppDrawer
+  MainContent.classList.toggle("app-drawer-shift"); // Acess this class in the CSS file and assign it MainContent
+  MainContent.classList.toggle("reduce-margin");
+  ScrollableHeader.classList.toggle("header-shift");
+});
